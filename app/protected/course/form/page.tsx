1767195d677aea0c15165a0751bef32b8c55d62a
CharanSala <salacharan81@gmail.com>
2025-05-10 21:17:33 +0530

    use the variables 'error' and 'isloading'

--- app/protected/course/form/page.tsx
+++ app/protected/course/form/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "@/components/ui/Button"; 
+
 
 export default function CourseRegistrationForm() {
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-
-  // State for each input field
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [organizedBy, setOrganizedBy] = useState("");
@@ -55,7 +56,9 @@ export default function CourseRegistrationForm() {
   };
 
   return (
+    
     <div className="w-screen flex justify-center items-center bg-slate-400">
+         
       <form
         onSubmit={onsubmit}
         className="w-full ml-10 mr-10 md:ml-0 md:mr-0 md:w-1/2 mx-auto my-12 px-8 py-10 bg-blue-50 text-gray-800 shadow-2xl rounded-3xl space-y-10 border border-gray-200"
@@ -63,6 +66,12 @@ export default function CourseRegistrationForm() {
         <h2 className="text-4xl font-extrabold text-center text-indigo-700 mb-6">
           Course Registration
         </h2>
+
+        {error && (
+          <div className="bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded-lg mb-6 animate-pulse">
+            {error}
+          </div>
+        )}
 
         {/* Basic Info */}
         <div className="rounded-2xl pb-10 border border-gray-200 bg-white/70 shadow p-6 space-y-6">
@@ -198,12 +207,14 @@ export default function CourseRegistrationForm() {
 
         {/* Submit */}
         <div className="flex justify-end w-full">
-          <button
-            type="submit"
-            className="px-8 py-3 font-semibold bg-gradient-to-r from-purple-500 to-indigo-600 text-white rounded-full shadow-lg hover:from-purple-600 hover:to-indigo-700 transition"
-          >
-            Submit
-          </button>
+        <Button
+                type="submit"
+                isLoading={isLoading}
+                size="lg"
+                className="px-8 py-3 font-semibold bg-gradient-to-r from-purple-500 to-indigo-600 text-white rounded-full shadow-lg hover:from-purple-600 hover:to-indigo-700 transition"
+              >
+                Submit
+              </Button>
         </div>
       </form>
     </div>
