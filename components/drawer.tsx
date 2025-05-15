a793135feadbdd0f44b20d8ed96cff8995e49b6a
CharanSala <salacharan81@gmail.com>
2025-05-15 18:28:44 +0530

    add course report in the taskbar and place courses at the bottom

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -49,27 +49,7 @@ const Drawer: React.FC = () => {
                             </ul>
                         </div>
 
-                        <div className="p-3 rounded-md bg-blue-800">
-                            <h3 className="text-lg font-semibold mb-2 text-white">Course</h3>
-                            <ul className="flex flex-col gap-4">
-                                <li>
-                                    <Link
-                                        href="/protected/course/form"
-                                        className="block bg-cyan-500 text-white py-2 px-4 rounded-md hover:bg-cyan-600 transition-colors"
-                                    >
-                                        Form
-                                    </Link>
-                                </li>
-                                {/* <li>
-                                    <Link
-                                        href="/protected/school/report"
-                                        className="block bg-cyan-500 text-white py-2 px-4 rounded-md hover:bg-cyan-600 transition-colors"
-                                    >
-                                        Report
-                                    </Link>
-                                </li> */}
-                            </ul>
-                        </div>
+                       
 
                         <div className="p-3 rounded-md bg-blue-800">
                             <h3 className="text-lg font-semibold mb-2 text-white">Student</h3>
@@ -161,6 +141,30 @@ const Drawer: React.FC = () => {
                                 </li>
                             </ul>
                         </div>
+
+                         <div className="p-3 rounded-md bg-blue-800">
+                            <h3 className="text-lg font-semibold mb-2 text-white">Course</h3>
+                            <ul className="flex flex-col gap-4">
+                                <li>
+                                    <Link
+                                        href="/protected/course/form"
+                                        className="block bg-cyan-500 text-white py-2 px-4 rounded-md hover:bg-cyan-600 transition-colors"
+                                    >
+                                        Form
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/protected/course/report"
+                                        className="block bg-cyan-500 text-white py-2 px-4 rounded-md hover:bg-cyan-600 transition-colors"
+                                    >
+                                        Report
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        
                     </div>
                     <div className="mt-auto mb-4 p-4">
                         <button
